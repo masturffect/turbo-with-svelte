cfc0eba435ac51f349a0599d7abfb67651276cab
Ian Seal <ianseal@Ians-MacBook-Air.local>
2023-04-20 15:08:45 -0400

    added new card components and docs storybook integrations

---
+++ libs/frontend/components/Button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import './button.css';
+
+  export let primary = false;
+
+  export let backgroundColor: string | undefined = undefined;
+
+  export let size: 'small' | 'medium' | 'large' = 'medium';
+
+  export let label: string = 'BUTTON';
+
+  $: mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+  $: style = backgroundColor ? `background-color: ${backgroundColor}` : '';
+</script>
+
+<button
+  type="button"
+  class={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+  {style}
+  on:click
+>
+  {label}
+</button>
---
+++ libs/frontend/components/Card.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import Button from "./Button.svelte";
+
+    export let subheader: String = ''
+    export let headline: String = ''
+    export let hasButton: Boolean = false
+
+</script>
+
+<div class="card-wrapper">
+    <div class="card-text-wrapper">
+        <span class="card-subheader">{subheader}</span>
+        <br />
+        <span class="card-headline">{headline}</span>
+    </div>
+    <div>
+        {#if hasButton}
+            <Button />
+        {/if}
+    </div>
+
+</div>
+
+<style>
+    .card-wrapper {
+        outline: solid 1px;
+        padding: 20px;
+        max-width: 200px;
+        height: 200px
+    }
+    .card-text-wrapper {
+        margin-bottom: 20px;
+        height: 30%;
+    }
+    .card-subheader {
+        font-size: 16px;
+        font-family: Arial, Helvetica;
+        color: grey;
+    }
+    .card-headline {
+        font-size: 24px;
+        font-family: Arial, Helvetica, sans-serif;
+        
+    }
+</style>
